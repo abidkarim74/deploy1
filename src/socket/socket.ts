61f769f3a0483b49fd06b57a2b4b2f07af544abf
abidkarim74 <karimabid737@gmail.com>
2025-03-29 20:36:25 +0500

    Update socket.ts

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://front-55nsi3qlh-abidkarim74s-projects.vercel.app", // Use environment variable
+    origin: "https://front-ealuuv8o7-abidkarim74s-projects.vercel.app", // Use environment variable
 
     methods: ["GET", "POST", "OPTIONS"],
 
