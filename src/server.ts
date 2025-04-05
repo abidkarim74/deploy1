e9505be1d8174408feb9948dd02d506875a1caa1
abidkarim <abidkarim737@gmail.com>
2025-04-05 14:25:12 +0500

    Updated

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://front-production-f68d.up.railway.app",
+  origin: "http://localhost:5173",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true 
 }));
--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://front-production-f68d.up.railway.app"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "OPTIONS"],
 
   },
